587713773cd96308a15ef81989d49eb6329f7406
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-22 23:48:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,3 +6,9 @@ def reverse_each_word(string)
   end
   return_array.join(" ")
 end
+
+def reverse_each_word(string)
+  return_val = []
+  string.split.each { |word| return_val << word.reverse }
+end
+end
