04b5d0f9c533cc7a3eee599a327151d1852398ab
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-22 23:48:50 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
-def reverse_each_word(string)
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
 
 def reverse_each_word(string)
   return_val = []
