357241a97347de63975927acc1e1ad79ad5a6560
Jesse Smokovich <jessesmokovich@yahoo.com>
2019-03-23 00:19:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,5 +7,5 @@ def reverse_each_word(sentence)
 end
 
 def reverse_each_word(sentence)
-  sentence.split.collect {|word| word.reverse}.join(" ")
+  sentence.split.collect {|word| word.reverse}.join(", ")
 end
